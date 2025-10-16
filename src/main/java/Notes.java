ca50a8eaf815fd1c273222a8029377a67fd1bf64
James Kalisz <james.kalisz@tdsb.on.ca>
2025-10-16 14:32:50 -0400

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -2,5 +2,24 @@
 class Notes {
 	public static void main(String[] args) {
 
+
+		int[] numArray = {3, 8, 3, 4, 6, 9, 1, 2, 6, 55, 44, 33};
+
+		for (int i = 1; i < numArray.length; i++) {
+			//go through the array
+			//set our number - get a copy of it
+			int key = numArray[i];
+			int index = i - 1;
+			while (index >= 0 && numArray[index] > key) {
+				numArray[index+1] = numArray[index];
+				index--;
+			}
+			numArray[index + 1] = key;
+		}
+
+
+		for (int i = 0; i < numArray.length; i++) {
+			System.out.print(numArray[i] + " ");
+		}
 	}
 }
